30a20be888f9b51758ee2065c37ff2ae79e98430
Roberto Ardila <jrag512@hotmail.com>
2022-06-24 12:26:18 -0500

    * More adjustments

--- Assets/TechDemo/Scripts/InputProviders/PlayerJoystickInputProvider.cs
+++ Assets/TechDemo/Scripts/InputProviders/PlayerJoystickInputProvider.cs
@@ -7,13 +7,11 @@ public class PlayerJoystickInputProvider : MonoBehaviour, IInputProvider
 	[SerializeField] private FireButton fireButton = null;
 	[SerializeField] private JumpButton jumpButton = null;
 
-	public void GetRawInput(out float forwardMovement, out float rightMovement, out bool fire, out bool jump, out float mouseX, out float mouseY)
+	public void GetRawInput(out float forwardMovement, out float rightMovement, out bool fire, out bool jump)
 	{
 		forwardMovement = Mathf.Clamp(joystick.Vertical + Input.GetAxis("Vertical"), -1, 1);
 		rightMovement = Mathf.Clamp(joystick.Horizontal + Input.GetAxis("Horizontal"), -1, 1);
 		fire = fireButton.IsPressed || Input.GetMouseButton(0);
 		jump = jumpButton.IsPressed || Input.GetKeyDown(KeyCode.Space);
-		mouseX = Input.mousePosition.x;
-		mouseY = Input.mousePosition.y;
 	}
 }
--- Assets/TechDemo/Scripts/PlayerInputController.cs
+++ Assets/TechDemo/Scripts/PlayerInputController.cs
@@ -18,16 +18,13 @@ namespace TechDemo
 		// Handling only one player through this input handlers, every player has the same player input controller
 		public void OnInputForClient(IInputWriter inputSerializer)
 		{
-			joystickInputProvider.GetRawInput(out var forwardMovement, out var rightMovement, out var fire, out var jump, out var mouseX, out var mouseY);
-			Vector3 pos = myMousePos;//FindPosHit();
+			joystickInputProvider.GetRawInput(out var forwardMovement, out var rightMovement, out var fire, out var jump);
+			Vector3 pos = myMousePos;
 			SerializeInput(inputSerializer, forwardMovement, rightMovement, fire, jump, pos.x, pos.y,pos.z);
 		}
 
 		public void OnInputForBot(IInputWriter inputSerializer)
 		{
-			//botInputProvider.GetRawInput(out var forwardMovement, out var rightMovement, out var fire, out var jump, out var mouseX, out var mouseY);
-			//Vector3 pos = FindPosHit();
-			//SerializeInput(inputSerializer, forwardMovement, rightMovement, fire, jump, pos.x, pos.y,pos.z);
 		}
 
 		private void Update()
@@ -35,30 +32,16 @@ namespace TechDemo
 			myMousePos = Input.mousePosition;
 		}
 
-		public Vector3 FindPosHit(Vector3 mousePos)
-        {
-			Vector3 result = Vector3.zero;
-            RaycastHit hit;
-            if (Physics.Raycast(cam.ScreenPointToRay(mousePos), out hit, 1000))
-            {
-                _playerBehaviour.characterAnimator.transform.LookAt(new Vector3(hit.point.x, 0, hit.point.z));
-                return _playerBehaviour.characterAnimator.transform.localEulerAngles;
-            }
-            return result;
-		}
 
-		private static void SerializeInput(IInputWriter inputWriter, float forwardMovement, float rightMovement, bool fire, bool jump, float rotX, float rotY, float rotZ)
+		private static void SerializeInput(IInputWriter inputWriter, float forwardMovement, float rightMovement, bool fire, bool jump, float posX, float posY, float posZ)
 		{
 			inputWriter.Write(forwardMovement);
 			inputWriter.Write(rightMovement);
 			inputWriter.Write(fire);
 			inputWriter.Write(jump);
-			inputWriter.Write(rotX);
-			inputWriter.Write(rotY);
-			inputWriter.Write(rotZ);
-			//inputWriter.Write(rotX);
-			//inputWriter.Write(rotY);
-			//inputWriter.Write(rotZ);
+			inputWriter.Write(posX);
+			inputWriter.Write(posY);
+			inputWriter.Write(posZ);
 		}
 
 		public void ElympicsUpdate()
@@ -70,9 +53,9 @@ namespace TechDemo
 			inputReader.Read(out float rightMovement);
 			inputReader.Read(out bool fire);
 			inputReader.Read(out bool jump);
-			inputReader.Read(out float rotX);
-			inputReader.Read(out float rotY);
-			inputReader.Read(out float rotZ);
+			inputReader.Read(out float posX);
+			inputReader.Read(out float posY);
+			inputReader.Read(out float posZ);
 
 			if (fire)
 			{
@@ -85,7 +68,19 @@ namespace TechDemo
 			}
 
 			_playerBehaviour.Move(forwardMovement, rightMovement);
-			FindPosHit(new Vector3(rotX,rotY,rotZ));
+			FindPosHit(new Vector3(posX,posY,posZ));
+		}
+
+		public Vector3 FindPosHit(Vector3 mousePos)
+		{
+			Vector3 result = Vector3.zero;
+			RaycastHit hit;
+			if (Physics.Raycast(cam.ScreenPointToRay(mousePos), out hit, 1000))
+			{
+				_playerBehaviour.characterAnimator.transform.LookAt(new Vector3(hit.point.x, _playerBehaviour.characterAnimator.transform.position.y, hit.point.z));
+				return _playerBehaviour.characterAnimator.transform.localEulerAngles;
+			}
+			return result;
 		}
 
 		public void Initialize()
@@ -108,18 +103,5 @@ namespace TechDemo
 
 		private bool CurrentPlayerControlsThis() => Elympics.Player == _playerBehaviour.PredictableFor;
 		private bool IsServerAndThisIsPlayer0() => Elympics.Player == ElympicsPlayer.World && _playerBehaviour.PredictableFor == ElympicsPlayer.FromIndex(0);
-
-		//private void Update()
-		//{
-			
-		//	//	//if (!_cameraFollowing)
-		//	//	//	return;
-
-		//	//	//var playerTransform = _playerBehaviour.transform;
-		//	//	//var camTransform = cam.transform;
-
-		//	//	//camTransform.LookAt(playerTransform);
-		//	//}
-		//}
 	}
 }
